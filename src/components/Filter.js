9e5f717bc4962cff0b9100c809287b9a057a75d9
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-01-25 19:59:11 +0300

    coding

---
+++ src/components/Filter.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Filter({handleCategoryChange}) {
+  
+
+ 
+
+
+
+  return (
+    <div class="filter-wrapper">
+      <select  className="filter"  onChange={handleCategoryChange}>
+            <option value="All">Filter by category</option>
+            <option value="fertilizer">Fertilizer</option>
+            <option value="Herbicide">Herbicides</option>
+            <option value="pesticide">Pesticides</option>
+            <option value="multi-vitamin">Multi-vitamins</option>
+            <option value="machinery">Machinery</option>
+      </select>
+       {/* <btn className="filter-btn" >Search</btn> */}
+    </div>
+  )
+}
+
+export default Filter;
