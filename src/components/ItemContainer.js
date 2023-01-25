b7b9b669ca792a9074e609ff894658afbd36e829
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-01-25 21:19:09 +0300

    coding

---
+++ src/components/ItemContainer.js
@@ -0,0 +1,30 @@
+import React,{useState} from 'react'
+import Items from './Items'
+
+function ItemContainer({itemsToDisplay,pushItem}) {
+
+  
+
+
+
+  return (
+
+  
+   <div id="products" className="row">
+
+     {itemsToDisplay.map(item=>{
+      return <Items 
+      key={item.id}
+      title={item.title}
+      image={item.image}
+      price={item.price}
+      // newArray={newArray}
+      pushItem={pushItem}
+      />
+     })
+     }
+   </div>
+  )
+}
+
+export default ItemContainer;
