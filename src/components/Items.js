b7f0490c8f69d3e86d4fe141fcb22ae3e700d096
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-01-25 21:37:04 +0300

    coding

---
+++ src/components/Items.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+function Items({key,title,image,price,pushItem}) {
+  // const{title,image,price}=item;
+  
+
+  
+  function handleAddToCart(){
+    const data=({
+      "image":image,
+      "title":title,
+      "price":price
+      
+    })
+    pushItem(data)
+
+   
+  }
+
+
+
+  return (
+    <div className="col-4">
+        <img src={image}/>
+        <h3>{key}</h3>
+        <h4 className='item-title'>{title}</h4>
+        
+          <p className='item-price'>Ksh {price}</p>
+          <btn className="btn" onClick={handleAddToCart}>Add to cart</btn>
+         
+        </div>
+   
+  )
+}
+
+export default Items;
