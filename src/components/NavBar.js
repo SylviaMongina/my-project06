0b3eba2ed252350270968d02750cf69610510304
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-01-25 14:08:38 +0300

    coding

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,3 +7,21 @@ function NavBar({newArray}) {
   function handleClick(){
     setIsTrue(!isTrue)
   }
+  return (
+    <div className='nav-bar'>
+        <nav>
+          <ul id="menuItems">
+            <li><a id="goBack" href="./">Home</a></li>
+            <li><a  href="#products">Products</a></li>
+            <li><a  href="#contact">Contact</a></li>
+            <li className='cart-icon'><img onClick={handleClick} className='cart' src='./images/cart.png'></img></li>
+            {isTrue ? <Cart newArray={newArray}  /> : null}
+          </ul>
+        </nav>
+        {/* <img src="" alt=""/>
+        <img className="menu-icon" src="../public/images/menu.png" onClick="menutoggle()"/> */}
+    </div>
+  )
+}
+
+export default NavBar;
