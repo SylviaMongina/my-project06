1fe52e5bc578d19d7a46187303df8f2134d72468
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-01-25 17:31:43 +0300

    coding

---
+++ src/components/Body.js
@@ -0,0 +1,57 @@
+import React,{useState} from 'react'
+import ItemContainer from './ItemContainer'
+import Filter from './Filter';
+
+
+function Body({itemList,pushItem}) {
+
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+
+ 
+
+
+  // setPushedItem(newArray)
+  // console.log(pushedItem)
+
+
+
+
+  function handleCategoryChange(event) {
+    // console.log(event)
+    // console.log(event.target.value)
+    setSelectedCategory(event.target.value);
+
+  }
+
+    const itemsToDisplay = itemList.filter((item) => {
+      if (selectedCategory === "All") return true;
+
+
+      
+      
+      
+      return item.category === selectedCategory;
+    }
+    );
+    // console.log(itemsToDisplay)
+    // setItemList(itemsToDisplay)
+    
+    
+    function handleAddToCart(event){
+      console.log(event.target.id)
+
+    }
+
+  return (
+<div>
+<Filter handleCategoryChange={handleCategoryChange}/>
+  <div className="small-container">
+    <ItemContainer itemsToDisplay={itemsToDisplay}  pushItem={pushItem}/>
+  </div>
+</div>
+  )
+}
+
+export default Body;
