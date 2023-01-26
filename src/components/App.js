7d6d1fada4e6419f5a64569e17d054778389a040
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-01-26 06:25:10 +0300

    coding

---
+++ src/components/App.js
@@ -0,0 +1,66 @@
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
+import React,{useEffect,useState} from 'react';
+import '../App.css';
+import Banner from "./Banner"
+import Body from './Body'
+import Brands from './Brands';
+import Footer from './Footer';
+
+
+
+
+function App() {
+  const [itemList,setItemList]=useState([])
+  const [brandsList,setBrandsList]=useState([])
+  const [nwArray,setNewArray]=useState([])
+
+  const newArray=[]
+
+
+
+
+  useEffect(()=>{
+    fetch('https://wakeful-steadfast-botany.glitch.me/mainData')
+      .then((resp)=>resp.json())
+      .then((items)=>setItemList(items))
+    
+
+  },[]);
+  useEffect(()=>{
+    fetch("https://wakeful-steadfast-botany.glitch.me/Brands")
+    .then((resp)=>resp.json())
+    .then((data)=>setBrandsList(data))
+  },[]);
+
+  function pushItem(item){
+    newArray.push(item)
+    console.log(newArray)
+    
+    // setPushedItem(newArray)
+    // console.log(pushedItem)
+
+  }
+  
+  // setNewArray(newArray)
+
+  return (
+    <div className="App">
+
+      <Banner newArray={newArray}/>
+
+      <Body
+       itemList={itemList} 
+       setItemList={setItemList}
+        
+       pushItem={pushItem}/>
+      <Brands brandsList={brandsList}/>
+      {/* <Comment/> */}
+      <Footer/>
+      {/* <Cart newArray={newArray}/> */}
+      
+    </div>
+  );
+
+}
+
+export default App;
